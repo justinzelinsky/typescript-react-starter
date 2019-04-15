3d19c3acf205e7ba6985c33269f8bee6d5a0496a
Justin Zelinsky <jpzelinsky@gmail.com>
2019-04-15 19:01:46 -0400

    Remove babel dependency, cleanup webpack

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,25 @@
 import './App.scss';
 
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
 interface Props {
   name: string;
 }
 
-const App: FunctionComponent<Props> = ({ name }) => (
-  <div styleName="greeting">Hello, {name}</div>
-);
+const App: FunctionComponent<Props> = ({ name }) => {
+  const [clicks, updateClick] = useState(0);
+  const handleClick = () => updateClick(clicks + 1);
+
+  return (
+    <div className="app">
+      <span className="greeting">
+        Hello, {name}! You clicked {clicks} times!
+      </span>
+      <button onClick={handleClick} className="btn">
+        Click me!
+      </button>
+    </div>
+  );
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,69 +3,80 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
+const paths = {
+  source: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist')
+};
 const devMode = process.env.NODE_ENV !== 'production';
+const styleLoader = devMode ? 'style-loader' : MiniCssExtractPlugin.loader;
 
-module.exports = {
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    hot: true,
-    port: 9000
+const devServer = {
+  contentBase: paths.dist,
+  compress: true,
+  hot: true,
+  port: 9000
+};
+
+const entry = path.join(paths.source, 'index.tsx');
+
+const rules = [
+  {
+    test: /\.tsx?$/,
+    enforce: 'pre',
+    loader: 'tslint-loader'
   },
-  entry: './src/index.tsx',
-  module: {
-    rules: [
+  {
+    test: /\.tsx?$/,
+    loader: 'ts-loader',
+    exclude: /node_modules/
+  },
+  {
+    test: /\.scss$/,
+    use: [
+      styleLoader,
+      'css-loader',
       {
-        test: /\.tsx?$/,
-        enforce: 'pre',
-        loader: 'tslint-loader'
-      },
-      {
-        test: /\.tsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(s)?css$/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[name]--[local]--[hash:base64:5]'
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              data: '@import "src/styles/globals";',
-              sourceMap: true
-            }
-          }
-        ]
+        loader: 'sass-loader',
+        options: {
+          data: '@import "src/styles/globals";',
+          sourceMap: true
+        }
       }
     ]
-  },
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({template: './src/index.html'}),
-    new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
-    })
-  ],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
-    modules: [
-      'node_modules',
-      'src',
-      'src/components'
-    ]
   }
+];
+
+const output = {
+  filename: 'app.js',
+  path: paths.dist
+};
+
+const plugins = [
+  new CleanWebpackPlugin(),
+  new HtmlWebpackPlugin({ template: './src/index.html' })
+];
+
+if (!devMode) {
+  plugins.push(
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css'
+    })
+  );
+}
+
+const resolve = {
+  extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
+  modules: ['node_modules', 'src', 'src/components']
+};
+
+module.exports = {
+  devServer,
+  entry,
+  module: {
+    rules
+  },
+  output,
+  plugins,
+  resolve
 };
